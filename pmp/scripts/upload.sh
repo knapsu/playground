6819cd13d3f20d6b816be604c7aea033d78f9ce6
Krzysztof Knapik <knapsu@gmail.com>
2020-12-29 19:41:42 +0100

    plex scripts

---
+++ pmp/scripts/upload.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "${SCRIPT}")
+WORKDIR=${PWD}
+
+if [ $# -eq 0 ]; then
+  echo "Missing script arguments" >&2
+  exit 1
+fi
+OPTIONS=$(getopt -o '' -l scp,transfer -- "$@")
+if [ $? -ne 0 ] ; then
+  echo "Invalid script arguments" >&2
+  exit 2
+fi
+eval set -- "$OPTIONS"
+
+while true; do
+  case "$1" in
+    (--scp)
+      UPLOAD_SCP="true"
+      shift;;
+    (--transfer)
+      UPLOAD_TRANSFER="true"
+      shift;;
+    (--)
+      shift; break;;
+    (*)
+      break;;
+  esac
+done
+
+if [ ! -f *.AppImage ]; then
+  echo "Nothing to upload"
+  exit
+fi
+
+if [[ "${UPLOAD_TRANSFER}" == "true" ]]; then
+  echo "Uploading to https://transfer.sh/"
+  curl --upload-file *.AppImage -https://transfer.sh/
+  echo
+fi
+
+if [[ "${UPLOAD_SCP}" == "true" ]]; then
+  echo "Uploading to SCP server"
+  SCP_USER=${SCP_USER:?Missing SCP user variable}
+  SCP_SERVER=${SCP_SERVER:?Missing SCP server variable}
+  scp -i keys/id_rsa *.AppImage ${SCP_USER}@${SCP_SERVER}:${SCP_PATH}
+fi
